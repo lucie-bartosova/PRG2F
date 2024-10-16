2ea2f009ec30713a1dc8c8eef4ccac6ea7f3395d
lucie.bartosova <lucie.bartosova@infis.cz>
2024-10-16 13:13:19 +0200

    While - cvičení

--- src/WhileCyklus.java
+++ src/WhileCyklus.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class WhileCyklus {
     public static void main(String[] args) {
-        /*
         int i = 1;
         while(i <= 10) {
             System.out.println(i);
@@ -29,7 +28,6 @@ public class WhileCyklus {
             number = sc.nextInt();
 
         } while (number <= 0);
-        */
 
 
         // Úkol 1 - Sudé čísla 0-1000
@@ -41,12 +39,50 @@ public class WhileCyklus {
 
 
         // Úkol 2 - hod kosktou
-        Random kostka = new Random();
-        int cislo;
+        Random kostka2 = new Random();
+        int cislo2;
         System.out.println("Házej, dokud nebude číslo 6");
         do {
-            cislo = kostka.nextInt(1,7);
-            System.out.println("hodil jsi "+cislo);
-        } while (cislo != 6);
+            cislo2 = kostka2.nextInt(1,7);
+            System.out.println("hodil jsi "+cislo2);
+        } while (cislo2 != 6);
+
+
+
+        // CV - lower/higher
+        Random random = new Random();
+        int vygenerovaneCislo = random.nextInt(0, 101); //dolní to generuje, horní ne)
+        int tipUzivatele;
+        do {
+            System.out.println("Zadejte Váš tip (číslo je v intervalu 0-100)"); //Psaní do stejné řádky: System.out.print("Zadejte Váš tip (číslo je v intervalu 0-100): ");
+            tipUzivatele = sc.nextInt();
+
+            if (tipUzivatele == vygenerovaneCislo) {
+                System.out.println("Trefa!!");
+            } else if (tipUzivatele > vygenerovaneCislo) {
+                System.out.println("Váš tip je moc vysoký.");
+            } else {
+                System.out.println("Váš tip je moc nízký.");
+            }
+
+        } while (tipUzivatele != vygenerovaneCislo); //while(true) - cyklus půjde do nekonečna a zastaví se tam, kde bude break; (například pokud bude tip správný)
+
+        //ukoly - soucet
+
+
+        int userCislo;
+        int soucet = 0;
+        do {
+            System.out.print("Zadejte číslo: ");
+            userCislo = sc.nextInt();
+            soucet += userCislo; // = soucet = userCislo + soucet;
+
+        } while (userCislo != 0);
+
+        System.out.println("Součet: "+soucet);
+
+
+
+
     }
 }
