07ba3f8420aca9a871e4571307c675e60edb0189
lucie.bartosova <lucie.bartosova@infis.cz>
2024-12-02 11:30:17 +0100

    String

--- src/StringyCV.java
+++ src/StringyCV.java
@@ -1,9 +1,12 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class StringyCV {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+
+        // jak je prvni slovo dlouhe
+        /*
 
         String veta = sc.nextLine();
         System.out.println(veta.indexOf(' '));
@@ -17,6 +20,65 @@ public class StringyCV {
         }
         System.out.println(pocet);
 
+
+
+
+        // String -> int -> String
+        System.out.print("Zadejte číslo: ");
+        String cislo = sc.next();
+
+        System.out.print("První číslo: " + cislo);
+        System.out.println();
+
+        int a = Integer.parseInt(cislo); // číslo se musí vejít do Int - jinak to spadne
+        a += 10;
+
+        System.out.print("Nové číslo: " + a);
+        System.out.println();
+
         sc.close();
+
+
+
+
+        // Kontrola začátku a konce Stringu
+        System.out.println("Zadejte text:");
+        String input = sc.nextLine();
+
+        boolean zacinaNaJava = input.startsWith("Java");
+        boolean konciNaRocks = input.endsWith("rocks.");
+
+        System.out.println("Řetězec začíná na \"Java\": " + zacinaNaJava);
+        System.out.println("Řetězec končí na \"rocks.\": " + konciNaRocks);
+
+
+
+        // Práce se substringy
+        String skocdopole = "Skočdopole";
+
+        //int begin = 0;
+        //skocdopole.substring(begin, begin+1)
+
+        // skocdopole.substring(7);  (od 0) - Vezme řetězec na od pozice, kterou zadám
+        System.out.println(skocdopole.substring(0, 4));
+        System.out.println(skocdopole.substring(4, 6));
+        System.out.println(skocdopole.substring(6, 10));
+
+
+        // Extrahovat část Stringu
+        System.out.print("Zadejte text: ");
+        String text = sc.nextLine();
+        System.out.print("Zadejte začátek (index): ");
+        int begin = sc.nextInt();
+        System.out.println("Zadejte konec (index): ");
+        int end = sc.nextInt();
+        System.out.println();
+
+        System.out.println(text.substring(begin, end));
+
+    */
+
+        // Řešení možných chyb
+
     }
 }
